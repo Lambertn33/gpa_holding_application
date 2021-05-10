0778f5a81d4ad64b7838aa35de62ce626be7d57e
Lambertn33 <lambertgithub@gmail.com>
2021-05-10 11:36:32 +0200

    Final FInalisation

--- resources/views/Dashboard/home/home.blade.php
+++ resources/views/Dashboard/home/home.blade.php
@@ -7,31 +7,26 @@
         <!--  Page-header opened -->
         <div class="page-header">
             <div class="page-leftheader">
-                <h1 class="page-title mb-0">ABOUT GPA Holdings Ltd INVOICES</h1>
+                {{-- <h1 class="page-title mb-0">GPA Holdings Ltd INVOICES</h1> --}}
             </div>
             <div class="page-rightheader">
-                <ol class="breadcrumb">
+                {{-- <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="#" class="d-flex"><span class="breadcrumb-icon">Pages</span></a></li>
                     <li class="breadcrumb-item active" aria-current="page">About</li>
-                </ol>
+                </ol> --}}
             </div>
         </div>
         <!--  Page-header closed -->
 
         <!-- row opened -->
-        <div class="row">
+        <div class="row" style="padding:20px">
             <div class="col-md-12">
                 <div class="card card-aside overflow-hidden">
-                    <a href="#" class="card-aside-column cover-image" data-image-src="/Dashboard/images/photos/20.jpg"></a>
                     <div class="card-body  flex-column">
-                        <h2 class="mb-2 font-weight-semibold fs-35">Why Choose US?</h2>
-                        <h4 class="leading-normal">majority have suffered alteration in some form, by injected</h4>
-                        <p class="leading-normal text-muted">There are many variations of passages of Lorem Ipsum available, but the majority have suffered by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of
-                            Lorem Ipsum, you need to as necessary All the Lorem Ipsum generators on the Internet tend to repeat Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and
-                            the like).</p>
-                        <p class="leading-normal">There are many variations of passages of Lorem Ipsum available, but the majority have suffered by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of
-                            Lorem Ipsum, you need to as necessary All the Lorem Ipsum generators on the Internet tend to repeat Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and
-                            the like).</p>
+                        <h2 class="mb-2 font-weight-semibold fs-35">ABOUT GPA Holdings Ltd INVOICES</h2>
+                        <p class="leading-normal text-muted">It helps you record your proforma and invoices.</p>
+                        <p class="leading-normal">It gives gives you the clients' database</p>
+                        <p class="leading-normal">It provides you with weekly, monthly and yearly reports.</p>
                     </div>
                 </div>
             </div>
